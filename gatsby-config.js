749aec7187667eb69bb4f3bdb453207d561b14da
unknown <bmfink87@gmail.com>
2023-03-28 05:00:34 -0400

    manifest isn't recognisizng ico, changed to svg

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       resolve: `gatsby-omni-font-loader`,
       resolve: 'gatsby-plugin-manifest',
       options: {
-        "icon": "src/images/mountain-solid.ico",
+        "icon": "src/images/mountain-solid.svg",
         enableListener: true,
         preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
         web: [
